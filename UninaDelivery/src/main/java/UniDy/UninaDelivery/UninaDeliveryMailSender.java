7c4f4064843b3ef80c1272705110cf75dd3fa2fb
Luigi2103 <lu.differente@studenti.unina.it>
2024-01-29 17:46:12 +0100

    Modifica settaggio mail

--- UninaDelivery/src/main/java/UniDy/UninaDelivery/UninaDeliveryMailSender.java
+++ UninaDelivery/src/main/java/UniDy/UninaDelivery/UninaDeliveryMailSender.java
@@ -3,7 +3,8 @@ package UniDy.UninaDelivery;
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
+import org.apache.commons.mail.HtmlEmail;
+
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -16,138 +17,145 @@ public class UninaDeliveryMailSender {
 
     protected void leggiCredenziali() throws IOException {
         BufferedReader buffer = new BufferedReader(new FileReader(new File("src/main/java/File/mail.txt")));
-            mail = buffer.readLine();
-            password = buffer.readLine();
-       
+        mail = buffer.readLine();
+        password = buffer.readLine();
     }
 
     protected void mandaMailaCliente(Cliente cliente) throws EmailException, IOException {
         leggiCredenziali();
-
-            // Creazione di un oggetto Email (si utilizza SimpleEmail per un'email semplice)
-            Email email = new SimpleEmail();
-
-            // Configurazione dei dettagli del server SMTP di Outlook
-            email.setHostName("smtp-mail.outlook.com");
-            email.setSmtpPort(587);
-
-            // Autenticazione con un nome utente e una password
-            email.setAuthenticator(new DefaultAuthenticator(mail, password));
-
-            // Abilita l'uso di TLS per la connessione sicura
-            email.setStartTLSEnabled(true);
-
-            // Indirizzo email del mittente
-            email.setFrom(mail);
-
-            // Indirizzo email del destinatario
-            email.addTo(cliente.getEmail());
-
-            // Oggetto dell'email
-            email.setSubject("Grazie per l'iscrizione a UninaDelivery");
-
-            // Corpo del messaggio dell'email
-            String messaggio = "Benvenuto/a a UninaDelivery, " + cliente.getNome() + " " + cliente.getCognome() + "!\n\n" +
-                    "Grazie per esserti iscritto/a a UninaDelivery! Siamo felici di averti con noi.\n" +
-                    "\nEcco alcune informazioni sul tuo profilo:\n" +
-                    "Codice Fiscale: " + cliente.getCodiceFiscale() + "\n" +
-                    "Data di Nascita: " + cliente.getDataNascita() + "\n" +
-                    "Numero di Telefono: " + cliente.getNumeroCellulare() + "\n" +
-                    "Preferenza di Contatto: " + cliente.getPreferenzaContatto() + "\n\n" +
-                    "Cordiali saluti,\nLo staff UninaDelivery";
-
-            email.setMsg(messaggio);
-
-            // Invia l'email
-            email.send();
-         
+        
+        // Creazione di un oggetto HtmlEmail
+        Email email = new HtmlEmail();
+        
+        // Configurazione dei dettagli del server SMTP di Outlook
+        email.setHostName("smtp-mail.outlook.com");
+        email.setSmtpPort(587);
+        
+        // Autenticazione con un nome utente e una password
+        email.setAuthenticator(new DefaultAuthenticator(mail, password));
+        
+        // Abilita l'uso di TLS per la connessione sicura
+        email.setStartTLSEnabled(true);
+        
+        // Indirizzo email del mittente
+        email.setFrom(mail);
+        
+        // Indirizzo email del destinatario
+        email.addTo(cliente.getEmail());
+        
+        //subject della mail
+        email.setSubject("Grazie per l'iscrizione a UninaDelivery");
+        
+        //messaggio
+        String messaggio = "<html><body>" +
+                "<p>Benvenuto/a a UninaDelivery, " + cliente.getNome() + " " + cliente.getCognome() + "!</p>" +
+                "<p>Grazie per esserti iscritto/a a UninaDelivery! Siamo felici di averti con noi.</p>" +
+                "<p>Ecco alcune informazioni sul tuo profilo:</p>" +
+                "<ul>" +
+                "<li>Codice Fiscale: " + cliente.getCodiceFiscale() + "</li>" +
+                "<li>Data di Nascita: " + cliente.getDataNascita() + "</li>" +
+                "<li>Numero di Telefono: " + cliente.getNumeroCellulare() + "</li>" +
+                "<li>Preferenza di Contatto: " + cliente.getPreferenzaContatto() + "</li>" +
+                "</ul>" +
+                "<p>Cordiali saluti,<br/>Lo staff UninaDelivery</p>" +
+                "</body></html>";
+        
+        //setta il messaggio
+        email.setMsg(messaggio);
+        
+        //manda la mail
+        email.send();
     }
-   
-    
-	protected void mandaMailAssunzioneCorriere(Corriere corriere) throws EmailException, IOException {
-		leggiCredenziali();
 
-		// Creazione di un oggetto Email (si utilizza SimpleEmail per un'email semplice)
-		Email email = new SimpleEmail();
+    protected void mandaMailAssunzioneCorriere(Corriere corriere) throws EmailException, IOException {
+        leggiCredenziali();
 
-		// Configurazione dei dettagli del server SMTP di Outlook
-		email.setHostName("smtp-mail.outlook.com");
-		email.setSmtpPort(587);
+     // Creazione di un oggetto HtmlEmail
+        Email email = new HtmlEmail();
+        
+        // Configurazione dei dettagli del server SMTP di Outlook
+        email.setHostName("smtp-mail.outlook.com");
+        email.setSmtpPort(587);
+        
+        // Autenticazione con un nome utente e una password
+        email.setAuthenticator(new DefaultAuthenticator(mail, password));
+        
+        // Abilita l'uso di TLS per la connessione sicura
+        email.setStartTLSEnabled(true);
+        
+        // Indirizzo email del mittente
+        email.setFrom(mail);
+        
+        // Indirizzo email del destinatario
+        email.addTo(corriere.getEmail());
+        
+        //subject della mail
+        email.setSubject("Grazie per l'iscrizione a UninaDelivery");
+        
+        //messaggio
+        String messaggio = "<html><body>" +
+                "<p>Gentile " + corriere.getNome() + " " + corriere.getCognome() + ",</p>" +
+                "<p>Siamo felici di informarti che sei stato assunto presso UninaDelivery come corriere.</p>" +
+                "<p>Dettagli dell'assunzione:</p>" +
+                "<ul>" +
+                "<li>Patenti possedute: " + corriere.getPatente() + "</li>" +
+                "<li>Salario: &euro;" + corriere.getContratto() + "</li>" +
+                "<li>Recapito contatto: " + corriere.getNumeroCellulare() + "</li>" +
+                "<li>Email: " + corriere.getEmail() + "</li>" +
+                "</ul>" +
+                "<p>Benvenuto nel nostro team!</p>" +
+                "<p>Cordiali saluti,<br/>Lo staff UninaDelivery</p>" +
+                "</body></html>";
 
-		// Autenticazione con un nome utente e una password
-		email.setAuthenticator(new DefaultAuthenticator(mail, password));
 
-		// Abilita l'uso di TLS per la connessione sicura
-		email.setStartTLSEnabled(true);
+        //setta il messaggio
+        email.setMsg(messaggio);
+        
+        //manda la mail
+        email.send();
+    }
 
-		// Indirizzo email del mittente
-		email.setFrom(mail);
+    protected void informaStatoOrdineCambiato(Ordine ordineModificato) throws EmailException, IOException {
+        leggiCredenziali();
+        
+        // Creazione di un oggetto HtmlEmail
+        Email email = new HtmlEmail();
+        
+        // Configurazione dei dettagli del server SMTP di Outlook
+        email.setHostName("smtp-mail.outlook.com");
+        email.setSmtpPort(587);
+        
+        // Autenticazione con un nome utente e una password
+        email.setAuthenticator(new DefaultAuthenticator(mail, password));
+        
+        // Abilita l'uso di TLS per la connessione sicura
+        email.setStartTLSEnabled(true);
+        
+        // Indirizzo email del mittente
+        email.setFrom(mail);
+        
+        // Indirizzo email del destinatario
+        email.addTo(ordineModificato.getAcquirente().getEmail());
+        
+        //subject della mail
+        email.setSubject("Cambio Stato Ordine UninaDelivery");
+        
+        //messaggio
+        String messaggio = "<html><body>" +
+                "<p>Gentile " + ordineModificato.getAcquirente().getNome() + " " + ordineModificato.getAcquirente().getCognome() + ",</p>" +
+                "<p>Le informiamo che il suo ordine ha subito una variazione di stato:</p>" +
+                "<ul>" +
+                "<li>STATO ATTUALE: " + ordineModificato.getStatoOrdine() + "</li>" +
+                "<li>DATA ACQUISTO: " + ordineModificato.getDataE() + "</li>" +
+                "<li>DATA ARRIVO: " + ordineModificato.getDataConsegna() + "</li>" +
+                "</ul>" +
+                "<p>Cordiali saluti,<br/>Lo staff UninaDelivery</p>" +
+                "</body></html>";
 
-		// Indirizzo email del destinatario
-		email.addTo(corriere.getEmail());
-
-		// Oggetto dell'email
-		email.setSubject("Assunzione presso UninaDelivery");
-
-		// Corpo del messaggio dell'email
-		String messaggio = "Gentile " + corriere.getNome() + " " + corriere.getCognome() + ",\n\n"
-				+ "Siamo felici di informarti che sei stato assunto presso UninaDelivery come corriere.\n"
-				+ "Dettagli dell'assunzione:\n" + "- Patenti possedute: " + corriere.getPatente() + "\n" + "- Salario: "
-				+ corriere.getContratto() + "€\n\n" + "- Recapito contatto : " + corriere.getNumeroCellulare() + "\n\n"
-				+ "- mail: " + corriere.getEmail() + "\n\n";
-
-		// if(corriere.getCodiceFiscaleCordinatore() != null)
-		// messaggio = messaggio + "\n\nPuoi chiedere iformazioni al tuo coordinatore:
-		// "+ corriere.getCodiceFiscaleCordinatore();
-
-		messaggio = messaggio + "\n\nBenvenuto nel nostro team!\n\n" + "Cordiali saluti,\nLo staff UninaDelivery";
-
-		email.setMsg(messaggio);
-
-		// Invia l'email
-		email.send();
-	}
-
-	protected void informaStatoOrdineCambiato(Ordine ordineModificato) throws EmailException, IOException {
-		leggiCredenziali();
-
-		// Creazione di un oggetto Email (si utilizza SimpleEmail per un'email semplice)
-		Email email = new SimpleEmail();
-
-		// Configurazione dei dettagli del server SMTP di Outlook
-		email.setHostName("smtp-mail.outlook.com");
-		email.setSmtpPort(587);
-
-		// Autenticazione con un nome utente e una password
-		email.setAuthenticator(new DefaultAuthenticator(mail, password));
-
-		// Abilita l'uso di TLS per la connessione sicura
-		email.setStartTLSEnabled(true);
-
-		// Indirizzo email del mittente
-		email.setFrom(mail);
-
-		// Indirizzo email del destinatario
-		email.addTo(ordineModificato.getAcquirente().getEmail());
-
-		// Oggetto dell'email
-		email.setSubject("Cambio Stato Ordine UninaDelivery");
-
-		// Corpo del messaggio dell'email
-		String messaggio = "Gentile "+ ordineModificato.getAcquirente().getNome()+ " " + ordineModificato.getAcquirente().getCognome()+".\nLe informiamo che il suo ordine ha subito una variazione di stato \n\n "
-				+ "-STATO ATTUALE :  "+ordineModificato.getStatoOrdine()
-				+ "\n -DATA ACQUISTO : "+ordineModificato.getDataE()+""
-				+ "\n -DATA ARRIVO : "+ordineModificato.getDataConsegna();
-		
-	
-		messaggio = messaggio + "\n\nCordiali saluti,\nLo staff UninaDelivery";
-
-		email.setMsg(messaggio);
-
-		// Invia l'email
-		email.send();
-
-	}
-    
-    
+        //setta il messaggio
+        email.setMsg(messaggio);
+        
+        //manda la mail
+        email.send();
+    }
 }
